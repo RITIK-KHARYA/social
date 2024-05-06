6b5837dab950567a07c91eefe0291f0a5590d8c3
ritik <ritikkharya05@gmail.com>
2024-05-06 17:27:47 +0530

    updated the message page with comment dialoge box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ import { TextGenerateEffect } from "@/components/ui/text-generate";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 export default async function Home() {
-  const user = await currentUser()
-  if(user){
-    return redirect("/home")
+  const user = await currentUser();
+  if (user) {
+    return redirect("/home");
   }
   return (
     <main className="flex flex-col items-center h-full  gap-y-4 dark:bg-[#1c1c1c] bg-white ">
       <div className="flex flex-col items-center justify-center h-screen gap-y-8 ">
-        <h1 className="text-6xl text-[#4CCD99] font-bold filter">
-          Welcome to Socials!
+        <h1 className="text-6xl text-[#4CCD99] font-bold filter cursor-default">
+          <b className="text-6xl text-[#CFCFCF] font-bold filter">Welcome to</b>{" "}
+          Socials!
         </h1>
-        <TextGenerateEffect words="Socials is a platform where you can share your thoughts and ideas with people around the world" className="text-5xl text-slate-300/90 text-center text-wrap w-[60%]" />
-        
+        <TextGenerateEffect
+          words="Socials is a platform where you can share your thoughts and ideas with people around the world"
+          className="text-5xl text-slate-300/90 text-center text-wrap w-[60%]"
+        />
+
         <div className="flex  items-center justify-center  gap-x-4 gap-y-6 py-2">
           <SignInButton>
             <Button>Sign In</Button>
